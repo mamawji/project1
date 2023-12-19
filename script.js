4a79bf1a5458ca8269672ef68f887b34cf4565dc
Mohamed Abbas Mawji <129640237+mamawji@users.noreply.github.com>
2023-12-19 16:17:28 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+
+function addTask(){
+    if (inputBox.value.trim() === '' ){
+        alert("Write something in the text box!");
+    }
+    else {
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value.trim();
+        listContainer.appendChild(li);
+
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+
+        inputBox.value = '';
+        saveData();
+    }
+    
+}
+
+listContainer.addEventListener("click", function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+        saveData();
+    }
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+        saveData()
+    }
+}, false)
+
+inputBox.addEventListener("keypress", function (e){
+    if(e.key === 'Enter'){
+        addTask();
+    }
+});
+
+function saveData(){
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+
+function showTasks(){
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+showTasks();
